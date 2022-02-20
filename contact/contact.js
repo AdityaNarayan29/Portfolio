0ad83420b1d2f51412c2e4d781f2733604477b87
AdityaNarayan2000 <29adityanarayan2000@gmail.com>
2022-02-20 18:56:16 +0530

    Update Transparencty

---
+++ contact/contact.js
@@ -0,0 +1,14 @@
+function sendEmail() {
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "29adityanarayan2000@gmail.com",
+    Password : "Aditya@123",
+    To : "29adityanarayan2000@gmail.com",
+    From : document.getElementById("email").value,
+    Subject : "A person wants to contact",
+    Body : document.getElementById("message").value
+}).then(
+  message => alert(message)
+);
+
+}
