40b2bc3714bd34f98cfdfec4add451f3aff1d1bc
AdityaNarayan2000 <29adityanarayan2000@gmail.com>
2022-06-25 16:50:52 +0530

    Skill update

--- contact/contact.js
+++ contact/contact.js
@@ -1,14 +1,13 @@
 function sendEmail() {
   Email.send({
-    Host : "smtp.gmail.com",
-    Username : "29adityanarayan2000@gmail.com",
-    Password : "Aditya@123",
-    To : "29adityanarayan2000@gmail.com",
-    From : document.getElementById("email").value,
-    Subject : "A person wants to contact",
-    Body : document.getElementById("message").value
-}).then(
-  message => alert(message)
-);
-
+    Host: "smtp.gmail.com",
+    Username: "29adityanarayan2000@gmail.com",
+    Password: "Aditya@123",
+    To: "29adityanarayan2000@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "A person wants to contact",
+    Body: document.getElementById("message").value
+  }).then(
+    message => alert(message)
+  );
 }
