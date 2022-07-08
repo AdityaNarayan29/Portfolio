b6e0bea938a91432926631995c1e23cc6dfcae52
AdityaNarayan2000 <29adityanarayan2000@gmail.com>
2022-07-08 13:05:44 +0530

    Preloader added

---
+++ Preloader/Preloader.js
@@ -0,0 +1,4 @@
+var preloader = document.getElementById('cssload-clock');
+function loader() {
+  preloader.style.display = 'none';
+}
--- Updates.md
+++ Updates.md
@@ -1,4 +1,3 @@
 ##about -> more should work
 ##skills -> bg update
 ##project -> button redirect, datoms
-##resume -> skill update
